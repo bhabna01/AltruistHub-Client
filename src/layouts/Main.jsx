79f9d25ddda5e794d3736342a493d0e095968b85
bhabna01 <abierhoque01@gmail.com>
2024-08-27 20:33:23 +0600

    loader added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,15 +1,31 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar/Navbar";
 import Footer from "../shared/Footer/Footer";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
 
 
 const Main = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate a loading delay (e.g., fetch data or load assets)
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000); // Adjust the delay as needed
+    }, []);
     return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
+        <>
+            {loading ? (
+                <Loader />
+            ) : (
+                <div >
+                    <Navbar></Navbar>
+                    <Outlet></Outlet>
+                    <Footer></Footer>
+                </div>
+            )}
+        </>
     );
 };
 
